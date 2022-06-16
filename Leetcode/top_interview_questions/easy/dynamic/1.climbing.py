807ddfb7b1266bff25de14551dd8661f3fdf1715
Farrukh Atabekov <augini@inbox.ru>
2022-06-16 16:44:11 +0900

    fix: leetcode climbing stairs

--- Leetcode/top_interview_questions/easy/dynamic/1.climbing.py
+++ Leetcode/top_interview_questions/easy/dynamic/1.climbing.py
@@ -1,7 +1,40 @@
-class Solution:
-    def climbStairs(n: int) -> int:
-       return "Hey there"
+class _Solution:   
+    # time limit exceeded when using this approach    
+     def _climbStairs(self, n: int) -> int:
+       #initialize the values    
+       queue = [[1], [2]]
+       sums = [1, 2]
+       combinations = []
        
-SolutionC = Solution()
+       while len(queue) > 0:
+           current = queue.pop(0)
+           current_sum = sums.pop(0)
+           
+           if current_sum == n:
+               combinations.append(current)
+               continue
+           
+           if current_sum + 1 <= n:
+               queue.append([*current, 1])
+               sums.append(current_sum+1)
+               
+           if current_sum + 2 <= n:
+               queue.append([*current, 2])
+               sums.append(current_sum+2)
+                
+       return len(combinations)
 
-print(Solution.climbStairs(3))
+class Solution:
+    # dynamic programming approach
+     def climbStairs(self, n: int) -> int:   
+       combinations = [1,1] # one way to climb zero starirs, one stair
+       
+       for x in range (2, n+1):
+           steps = combinations[x-1] + combinations[x-2]
+           combinations.append(steps)
+        
+       return combinations[n]
+   
+Sample = Solution()
+
+print(Sample.climbStairs(2))
