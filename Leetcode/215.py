2d7c55005e391932519a0db323df3273b0c8798b
Atabekov Farrukh <augini@inbox.ru>
2023-05-07 15:47:49 +0500

    fix: update solutions

--- Leetcode/215.py
+++ Leetcode/215.py
@@ -4,7 +4,11 @@ import heapq
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heapq.heapify(nums)
+        heapq.heapify(nums)  # O(logn)
+        # m
         while len(nums) > k:
-            heapq.heappop(nums)
+            heapq.heappop(nums)  # O(logn)
         return nums[0]
+
+
+# Time Comlexity O(m*logn)
--- Leetcode/973.py
+++ Leetcode/973.py
@@ -6,14 +6,27 @@ import heapq
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         heap = []
-        for x, y in points:
-            dist = (x**2) + y**2
-            heap.append([dist, x, y])
+        # n = len(points)
 
+        # O(n)
+        for x, y in points:
+            # calculate dist
+            dist = x**2 + y**2
+            heap.append((dist, x, y))
+
+        # O(log(n))
         heapq.heapify(heap)
+
         res = []
+        # O(k)
         while k > 0:
             dist, x, y = heapq.heappop(heap)
             res.append((x, y))
             k -= 1
+
         return res
+
+
+# n -> len(points)
+# Time complexity O(n)
+# Space O(n)
