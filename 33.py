3e74db79e20ff4f854ecdfda821308224e900472
Atabekov Farrukh <augini@inbox.ru>
2023-05-25 16:04:29 +0500

    fix: add the second approach to Leetcode 33

--- 33.py
+++ 33.py
@@ -24,3 +24,32 @@ class Solution:
                     right = mid - 1
 
         return -1
+
+    def _search(self, nums: List[int], target: int) -> int:
+        # find the index of the breaking point and re rotate the array
+        start, end = 0, len(nums) - 1
+
+        while nums[start] > nums[end]:
+            mid = (start + end) // 2
+
+            if nums[mid] > nums[end]:
+                start = mid + 1
+            else:
+                end = mid
+
+        # re rotate
+        nums_sorted = [*nums[start : len(nums)], *nums[0:start]]
+
+        # do the binary search in a sorted list
+        start, end = 0, len(nums)
+
+        while start < end:
+            mid = (start + end) // 2
+            if nums_sorted[mid] < target:
+                start = mid + 1
+            elif nums_sorted[mid] > target:
+                end = mid
+            else:
+                return nums.index(nums_sorted[mid])
+
+        return -1
