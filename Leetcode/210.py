74247c12050ada9f8a51c51767475a63b9a60e0c
Atabekov Farrukh <augini@inbox.ru>
2023-06-17 22:03:11 +0500

    attempt: problem 210

--- Leetcode/210.py
+++ Leetcode/210.py
@@ -1,35 +1,37 @@
-# https://leetcode.com/problems/course-schedule/description/
+# https://leetcode.com/problems/course-schedule-ii/
+
 from collections import defaultdict
 from typing import List
 
 
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        if not len(prerequisites):
-            return True
-
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         edges = defaultdict(list)
+
         for a, b in prerequisites:
             edges[a].append(b)
 
         visited = set()
+        order = set()
 
         def dfs(edge):
             if edge in visited:
-                return False
+                return []
             if edges[edge] == []:
                 return True
 
             visited.add(edge)
             for crs in edges[edge]:
                 if not dfs(crs):
-                    return False
+                    return []
 
             visited.remove(edge)
             edges[edge] = []
             return True
 
         for i in range(numCourses):
+            order.add(i)
             if not dfs(i):
-                return False
-        return True
+                return []
+
+        return list(order)
